92433784b175e51753614a402ee6d314390046f4
NV-Phong <nv.phong.dev@gmail.com>
2026-03-25 20:14:48 +0700

    FEAT - ( close #1 ) : hue adjustment

--- code.ts
+++ code.ts
@@ -5,37 +5,111 @@
 
 // Runs this code if the plugin is run in Figma
 if (figma.editorType === 'figma') {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many rectangles on the screen.
+  figma.showUI(__html__, { width: 300, height: 200 });
 
-  // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
+  figma.ui.onmessage = msg => {
+    if (msg.type === 'change-hue') {
+      const targetHue = msg.hue; // Hue between 0 and 360
 
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      // This plugin creates rectangles on the screen.
-      const numberOfRectangles = msg.count;
-
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < numberOfRectangles; i++) {
-        const rect = figma.createRectangle();
-        rect.x = i * 150;
-        rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-        figma.currentPage.appendChild(rect);
-        nodes.push(rect);
+      const nodes = figma.currentPage.selection;
+      if (nodes.length === 0) {
+        figma.notify('Please select at least one object');
+        return;
       }
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    }
 
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+      function rgbToHsl(r: number, g: number, b: number): [number, number, number] {
+        const max = Math.max(r, g, b);
+        const min = Math.min(r, g, b);
+        let h = 0, s = 0, l = (max + min) / 2;
+
+        if (max !== min) {
+          const d = max - min;
+          s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+          switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+          }
+          h /= 6;
+        }
+
+        return [h * 360, s, l];
+      }
+
+      function hslToRgb(h: number, s: number, l: number): [number, number, number] {
+        let r, g, b;
+        h /= 360;
+
+        if (s === 0) {
+          r = g = b = l; // achromatic
+        } else {
+          const hue2rgb = (p: number, q: number, t: number) => {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1/6) return p + (q - p) * 6 * t;
+            if (t < 1/2) return q;
+            if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+          };
+
+          const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+          const p = 2 * l - q;
+          r = hue2rgb(p, q, h + 1/3);
+          g = hue2rgb(p, q, h);
+          b = hue2rgb(p, q, h - 1/3);
+        }
+
+        return [r, g, b];
+      }
+
+      function changeHueImpl(color: RGB, targetHue: number): RGB {
+        const [h, s, l] = rgbToHsl(color.r, color.g, color.b);
+        const [newR, newG, newB] = hslToRgb(targetHue, s, l);
+        return { r: newR, g: newG, b: newB };
+      }
+
+      for (const node of nodes) {
+        if ('fills' in node) {
+          const fills = clone(node.fills);
+          let changed = false;
+          for (const fill of fills) {
+            if (fill.type === 'SOLID') {
+              fill.color = changeHueImpl(fill.color, targetHue);
+              changed = true;
+            } else if (fill.type === 'GRADIENT_LINEAR' || fill.type === 'GRADIENT_RADIAL' || fill.type === 'GRADIENT_ANGULAR' || fill.type === 'GRADIENT_DIAMOND') {
+                for (let i = 0; i < fill.gradientStops.length; i++) {
+                   const c = fill.gradientStops[i].color;
+                   const newC = changeHueImpl({r: c.r, g: c.g, b: c.b}, targetHue);
+                   fill.gradientStops[i].color = { r: newC.r, g: newC.g, b: newC.b, a: c.a };
+                }
+                changed = true;
+            }
+          }
+          if (changed) {
+            node.fills = fills;
+          }
+        }
+        if ('strokes' in node) {
+          const strokes = clone(node.strokes);
+          let changed = false;
+           for (const stroke of strokes) {
+            if (stroke.type === 'SOLID') {
+              stroke.color = changeHueImpl(stroke.color, targetHue);
+              changed = true;
+            }
+          }
+          if (changed) {
+            node.strokes = strokes;
+          }
+        }
+      }
+
+      function clone(val: any) {
+        return JSON.parse(JSON.stringify(val));
+      }
+    } else if (msg.type === 'cancel') {
+      figma.closePlugin();
+    }
   };
 }
 
